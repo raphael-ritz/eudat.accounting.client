9cd31ff7f819d6d1aabd78105094b0c26f8c19ff
Raphael Ritz <raphael.ritz@gmail.com>
2017-02-22 15:39:34 +0100

    Imporove formatting a bit

--- src/eudat/accounting/client/iRODScollector.py
+++ src/eudat/accounting/client/iRODScollector.py
@@ -3,8 +3,6 @@
 ===============================
 eudat.accounting.iRODScollector
 ===============================
-
-Command line handling
 """
 
 import json
@@ -70,54 +68,56 @@ class Configuration(object):
 
 
 class EUDATAccounting(object):
-    """Class implementing the computation of statistics about resource consumption."""
+    """
+    Class implementing the computation of statistics about resource consumption.
+    """
 
     def __init__( self, conf, logger ):
-        """Initialize object with configuration parameters."""
-
+        """
+        Initialize object with configuration parameters.
+        """
         self.conf = conf
         self.logger = logger
 
     def _query_iCATDb(self):
-        """Query iCATdb for number of stored objects and used space in bytes"""
-
+        """
+        Query iCATdb for number of stored objects and used space in bytes
+        """
         collections = self.conf.collections.split()
         total_objects = 0
         total_space   = 0
         print "Collections to be accounted:"
         for collection in collections:
-            print collection
-
             try:
-                #query size of the collection in bytes
+                # query size of the collection in bytes
                 out=self._raw_query(collection,"DATA_SIZE","sum")
 
-                #Check that output is correct and starting with DATA_SIZE
+                # check that output is correct and starting with DATA_SIZE
                 if out[:9]=="DATA_SIZE":
                     out     = int(filter(str.isdigit, out))
                     total_space+=int(out)
-                    self.logger.info("Storage space for collection: "+collection+\
-                                                                ": "+str(out))
+                    self.logger.info("Storage space for collection: "\
+                                     +collection+": "+str(out))
                 else:
-                    self.logger.info("Wrong output for storage space in collection: "\
-                                                                    +collection)
+                    self.logger.warn("Wrong output for storage space "\
+                                     "in collection: "+collection)
 
-                #query number of objects of the collection
+                # query number of objects of the collection
                 out=self._raw_query(collection,"DATA_ID","count")
 
-                #Check that output is correct and starting with DATA_ID
+                # check that output is correct and starting with DATA_ID
                 if out[:7]=="DATA_ID":
                     out= int(filter(str.isdigit, out))
                     total_objects+=int(out)
                     self.logger.info("number of objects for collection "\
-                                                    +collection+": "+str(out))
+                                     +collection+": "+str(out))
                 else:
-                    self.logger.info("Wrong output for object number in collection: "\
-                                                                    +collection)
+                    self.logger.warn("Wrong output for object number "\
+                                     "in collection: "+collection)
 
             except Exception,e:
                 sys.stdout.write("Exception %s encountered!" % str(e))
-                self.logger.info("Exception %s encountered!" % str(e))
+                self.logger.warn("Exception %s encountered!" % str(e))
                 sys.exit(1)
 
         used_objects= total_objects
@@ -126,17 +126,20 @@ class EUDATAccounting(object):
         return used_objects, used_space
 
     def _raw_query(self,collection,data_type,db_func):
-
-        #query size of the collection in bytes
-        query_size="select "+db_func+"("+data_type+") where COLL_NAME like '"\
-                                            +collection+"%' |grep "+data_type
-        process = subprocess.Popen(["iquest",query_size], stdout=subprocess.PIPE)
+        """
+        construct query string and pipe it to iquest
+        """
+        query = "select %s(%s) where COLL_NAME like %s%% | grep %s" \
+                % (db_func, data_type, collection, data_type)
+        process = subprocess.Popen(["iquest",query], 
+                                   stdout=subprocess.PIPE)
         out,err = process.communicate()
         return out
 
     def _toAccountingRecord(self, stats):
-        """Format an eudat accounting record"""
-
+        """
+        Cast to format of an eudat accounting record
+        """
         return {
             'account': self.conf.account,
             'number': stats[0],
@@ -144,40 +147,53 @@ class EUDATAccounting(object):
         }
 
     def reportStatistics(self, args):
-        """ Report statistical data on resource consumption to remote server"""
-
+        """
+        Report statistical data on resource consumption to remote server
+        """
         data = self._query_iCATDb()
 
         acctRecords = []
         acctRecords.append(self._toAccountingRecord(data))
-        # this is a hack - adding the data to the args
-        # so other command line args resp their defaults 
-        # are available as well
+        # adding the data to the args so other command line args 
+        # resp their defaults are available as well
         args.account = acctRecords[0]['account']
         args.value = acctRecords[0]['value']
         args.number = acctRecords[0]['number']
-        #data = json.dumps(acctRecords)
         pretty_data = json.dumps(acctRecords, indent=4)
         self.logger.info('Data: ' + pretty_data)
 
         credentials = utils.getCredentials(self.conf)
+        self.logger.info("Credentials found")
+        self.logger.debug("Credentials: " + credentials)
         url = utils.getUrl(self.conf)
+        logger.info("URL to call: " + url)
         data = utils.getData(args)
+        logger.info("Data as query string: " + data)
 
         if args.test:
-            print "Test: Would send the following data: %s" % data
-        else:
-            response = utils.call(credentials, url, data)
-            self.logger.info('Data sent. Status code: ' + str(response.status_code))
-            print "\nData sent. Status code: " + str(response.status_code)
+            print "Test: Would send the following data: " \
+                + data
+            return None
 
+        response = utils.call(credentials, url, data)
+
+        self.logger.info('Data sent. Status code: ' \
+                         + str(response.status_code))
+        if args.verbose:
+            print "\nData sent. Status code: " \
+                + str(response.status_code)
+            print "Key of generated accounting record" \
+                + response.text
 
 
 def main(argv=sys.argv):
-    """Main function called from console command
     """
-    logging.basicConfig(filename='.accounting.log', level=logging.INFO,
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    Main function called from console command
+    """
+    logging.basicConfig(filename='.accounting.log', 
+                        level=logging.INFO,
+                        format='%(asctime)s - %(name)s \
+                        - %(levelname)s - %(message)s')
     exit_code = 1
     try:
         app = Application(argv)
@@ -191,9 +207,9 @@ def main(argv=sys.argv):
 
 
 class Application(ApplicationBase):
-    """The main Application class of the old iRODScollector
-    Signature is the same as for the simple client - except for 
-    the required arguments."""
+    """
+    The main Application class of the iRODScollector
+    """
 
     def run(self):
         LOG.info("iRODScollector called with: " + str(self.args))
@@ -205,7 +221,8 @@ class Application(ApplicationBase):
         logger = logging.getLogger('StorageAccounting')
         logger.setLevel(logging.INFO)
 
-        configuration = Configuration(self.args.configpath, logger, fileparser)
+        configuration = Configuration(self.args.configpath, 
+                                      logger, fileparser)
         configuration.parseConf()
 
         eurep = EUDATAccounting(configuration, logger)
