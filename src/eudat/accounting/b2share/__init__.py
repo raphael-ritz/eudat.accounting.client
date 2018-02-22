e808dc25aae3e196884ab5acbd032be57ea0fd67
jani.heikkinen <jani.heikkinen@csc.fi>
2018-02-22 18:18:37 +0200

    pep fix

--- src/eudat/accounting/b2share/__init__.py
+++ src/eudat/accounting/b2share/__init__.py
@@ -3,5 +3,5 @@ import pkg_resources
 try:
     __version__ = pkg_resources.get_distribution(u'eudat.accounting.b2share').version
 except:
-    #LOG.warning("Could not get the package version from pkg_resources")
+    # LOG.warning("Could not get the package version from pkg_resources")
     __version__ = 'unknown'
