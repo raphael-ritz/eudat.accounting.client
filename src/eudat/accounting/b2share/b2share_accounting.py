55d30a62d2b229f1f64b8ab84365dd7efbd861e1
jani.heikkinen <jani.heikkinen@csc.fi>
2018-02-26 16:51:50 +0200

    change to rest api

--- src/eudat/accounting/b2share/b2share_accounting.py
+++ src/eudat/accounting/b2share/b2share_accounting.py
@@ -18,50 +18,21 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import psycopg2
-import os
+import requests
 
 
 class B2SHAREAccounting(object):
 
-    def __init__(self, conf):
-        self.account = conf.account
-        self.dbname = os.environ.get('B2SHARE_POSTGRESQL_DBNAME', conf.db_name)
-        self.user = os.environ.get('B2SHARE_POSTGRESQL_USER', conf.db_user)
-        self.password = os.environ.get('B2SHARE_POSTGRESQL_PASSWORD', conf.db_password)
-        self.host = os.environ.get('B2SHARE_POSTGRESQL_HOST', conf.db_host)
-        self.db_connect_info = 'host={0} dbname={1} user={2} password={3}'.format(self.host, self.dbname,
-                                                                                  self.user, self.password)
+    def __init__(self, conf, logger):
+        self.logger = logger
+        self.url = conf.b2share_url
+        self.community = conf.b2share_community
 
     def report(self, args):
-        db_conn = None
-        accounting = {}
 
-        try:
-            db_conn = psycopg2.connect(self.db_connect_info)
+        url = self.url + "/api/records/?q=community:" + self.community
+        r = requests.get(url, verify=True)
+        if r.status_code != requests.codes.ok:
+            self.logger.warning('get community records status code: %d', r.status_code)
 
-            cur = db_conn.cursor()
-            cur.execute("select id,json from records_metadata;")
-
-            for record_id, record in cur:
-                if record is None:
-                        continue
-
-                community = record['community']
-                if community not in accounting:
-                        accounting[community] = 1
-                else:
-                        accounting[community] += 1
-            cur.close()
-
-        except psycopg2.Error, e:
-            print "Error", e
-
-        finally:
-            if db_conn:
-                db_conn.close()
-
-        if self.account in accounting:
-            return accounting[self.account], 0
-        else:
-            return 0, 0
+        return (r.json()['hits']['total'], 0) if (r.status_code == requests.codes.ok) else (0, 0)
